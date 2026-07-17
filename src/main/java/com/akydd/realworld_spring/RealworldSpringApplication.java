6355d93e33e1db1959af088a2e6635fb927ce376
Alan Kydd <kydd.alan.g@gmail.com>
2026-07-17 11:53:19 -0600

    Trying out Spring!

---
+++ src/main/java/com/akydd/realworld_spring/RealworldSpringApplication.java
@@ -0,0 +1,13 @@
+package com.akydd.realworld_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealworldSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akydd/realworld_spring/RealworldSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akydd.realworld_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
